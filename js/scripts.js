5bcfbfc68d5b8dfc08490051ed69260b4a16fd15
alex dinnel & David Torbeck <student@epicodus.com>
2014-02-24 17:26:35 -0800

    edits scripts file

--- js/scripts.js
+++ js/scripts.js
@@ -7,23 +7,49 @@ var Contact = {
 $(document).ready(function() {
   $('form#new-contact').submit(function() {
     event.preventDefault();
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
-    var inputtedAddress = $("input#new-address").val();
+    var clickedForEdit;
+    var inputFirstName = $("input#new-first-name").val();
+    var inputLastName = $("input#new-last-name").val();
+    var inputAddress = $("input#new-address").val();
     var newContact = Object.create(Contact);
-    newContact.firstName = inputtedFirstName;
-    newContact.lastName = inputtedLastName;
-    newContact.address = inputtedAddress;
+    newContact.firstName = inputFirstName;
+    newContact.lastName = inputLastName;
+    newContact.address = inputAddress;
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + '</span></li>');
 
     $(".contact").last().click(function(){
-      $("#show-contact").show();
-      $("#show-contact h2").text(newContact.fullName());
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
-      $(".address").text(newContact.address);
+      clickedForEdit = $(this);
+      $(".edit").show();
+      $("#edit-first-name").val(inputFirstName);
+      $("#edit-last-name").val(inputLastName);
+      $("#edit-address").val(inputAddress);
+      
+
+      $("#edit-contact").submit(function(event) {
+         event.preventDefault();
+         newContact.firstName = $("#edit-first-name").val();
+         newContact.lastName = $("#edit-last-name").val();
+         newContact.address = $("#edit-address").val();
+         clickedForEdit.html(newContact.fullName());
+         
+         clickedForEdit.click(function(event) {
+           $(".edit").show();
+           var inputFirstName = $("edit#new-first-name").val();
+           var inputLastName = $("edit#new-last-name").val();
+           var inputAddress = $("edit#new-address").val();
+           newContact.firstName = inputFirstName;
+           newContact.lastName = inputLastName;
+           newContact.address = inputAddress;
+  
+           // event.preventDefault();
+         });
+
+         
+      });
+      
     });
+    
 
     this.reset();
   })
